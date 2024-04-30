a8a9202ca95e5c3d0b19b51c3e06a2c6f0f72837
Murilogtss <104167403+Murilogtss@users.noreply.github.com>
2024-04-30 01:47:39 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </>
   );
 };
